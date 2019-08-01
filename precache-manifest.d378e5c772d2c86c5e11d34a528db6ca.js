d1dac3d078e0fc07bceefe361f1cec80d950b019
kingzmen <kingzmentechnology@gmail.com>
2019-08-01 09:10:29 +0100

    Updates

---
+++ precache-manifest.d378e5c772d2c86c5e11d34a528db6ca.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23e40377c779fd2b3c4893f078586a29",
+    "url": "/mywebsite/index.html"
+  },
+  {
+    "revision": "72b83a550a5b5a96357f",
+    "url": "/mywebsite/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "07cb943c24c60855dbc6",
+    "url": "/mywebsite/static/css/main.a1b7b015.chunk.css"
+  },
+  {
+    "revision": "72b83a550a5b5a96357f",
+    "url": "/mywebsite/static/js/2.f9427812.chunk.js"
+  },
+  {
+    "revision": "07cb943c24c60855dbc6",
+    "url": "/mywebsite/static/js/main.0e7a0ed8.chunk.js"
+  },
+  {
+    "revision": "1029a0e86ffa031e1963",
+    "url": "/mywebsite/static/js/runtime~main.8e5ad122.js"
+  },
+  {
+    "revision": "bf79b6757027c409ee6c6107ee2074ce",
+    "url": "/mywebsite/static/media/devgrub.bf79b675.png"
+  },
+  {
+    "revision": "3da49c686de364f2e513abcafa25f3ca",
+    "url": "/mywebsite/static/media/evverest.3da49c68.png"
+  },
+  {
+    "revision": "0f42315392f46e7b84119d08949ca303",
+    "url": "/mywebsite/static/media/youtube.0f423153.png"
+  }
+]);
